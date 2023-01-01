e355f4f818ff5d055f4a130e015830f134944175
Iryna Guravska <irinaguru.ua@gmail.com>
2023-01-02 01:43:07 +0200

    Add values to the variable computerMove

--- js/functions.js
+++ js/functions.js
@@ -2,12 +2,9 @@ function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 
-    document.getElementById('messages').appendChild(div);
+   document.getElementById('messages').appendChild(div);
 }
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-let computerMove = 'kamień';
-let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
