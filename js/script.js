d19db414c6b4fff13359500b250eaa2873b05410
Iryna Guravska <irinaguru.ua@gmail.com>
2023-01-05 19:16:08 +0200

    const  randomNumber

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function playGame(playerInput){
    }
    clearMessages ();
 
-   let randomNumber = Math.floor(Math.random() * 3 + 1);
+   const randomNumber = Math.floor(Math.random() * 3 + 1);
 
    console.log('Wylosowana liczba to: ' + randomNumber);
 
@@ -22,14 +22,11 @@ function playGame(playerInput){
 
 printMessage('Mój ruch to: ' + computerMove);
 
-/*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
-
    console.log('Gracz wpisał: ' + playerInput);
 
    let playerMove = getMoveName(playerInput);
 
    printMessage('Twój ruch to: ' + playerMove);
-
 
 function displayResult(argComputerMove, argPlayerMove) {
    console.log("moves:", argComputerMove, argPlayerMove);
@@ -41,7 +38,6 @@ function displayResult(argComputerMove, argPlayerMove) {
    ) {
    printMessage("Ty wygrywasz!");
    counterPlayer += 1;
-
    } else if (
       (argComputerMove == "papier" && argPlayerMove == "kamień") ||
       (argComputerMove == "kamień" && argPlayerMove == "nożyce") ||
@@ -49,7 +45,6 @@ function displayResult(argComputerMove, argPlayerMove) {
    ) {
    printMessage("Ja wygrywam!");
    counterComp += 1;
-
    } else if (
       (argComputerMove == "kamień" && argPlayerMove == "kamień") ||
       (argComputerMove == "papier" && argPlayerMove == "papier") ||
@@ -61,23 +56,22 @@ function displayResult(argComputerMove, argPlayerMove) {
    }
 }
 
-   displayResult (computerMove, playerMove);
+   displayResult(computerMove, playerMove);
    console.log(counterComp + ":" + counterPlayer);
    printMessage(counterComp + ":" + counterPlayer);
-
-   console.log ('hello');
 }
+
 document.getElementById('play-rock').addEventListener('click', function(){
    playGame(1);
- });
+});
 
- document.getElementById('play-paper').addEventListener('click', function(){
+document.getElementById('play-paper').addEventListener('click', function(){
    playGame(2);
- });
+});
 
- document.getElementById('play-scissors').addEventListener('click', function(){
+document.getElementById('play-scissors').addEventListener('click', function(){
    playGame(3);
- });
- 
- let counterComp = 0;
+});
+
+let counterComp = 0;
 let counterPlayer = 0;
